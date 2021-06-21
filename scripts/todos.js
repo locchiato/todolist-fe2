075f90828c787f54b461a85e540cb8f08f9e4127
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-06-21 05:15:15 -0300

    [MOD] Se intenta ingresar una tarea

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,35 +1,87 @@
-let listadoTodos = [{
+let todos = [{
     description: "Mi hermosa tarea",
     createdAt: "19/04/20",
 }, ];
 
-/* <li class="tarea">
+const tareasPendientes = document.querySelector(".tareas-pendientes");
+
+function renderizarTodos() {
+    //  console.log("deberiamos renderizar todas las todos del array");
+    //for (let i = 0; i < listadoTodos.length; i++) {
+    todos.forEach((todo) => {
+        tareasPendientes.innerHTML += `<li class="tarea">
+            <div class="not-done"></div>
+            <div class="descripcion">
+            <p class="nombre">${todo.description}</p>
+            <p class="timestamp">${todo.createdAt}</p>
+            </div>
+            </li>
+            `;
+    })
+
+}
+
+
+function agregarTodo() {
+    const inputValue = document.querySelector('.nueva-tarea input').value.trim();
+    if (inputValue.length) {
+        console.log(...todos)
+        let nuevaLista = []
+        const todo = {
+            description: inputValue,
+            createdAt: (new Date(Date.now())).toLocaleDateString()
+        }
+        nuevaLista.push(todo);
+        todos = todos.concat(nuevaLista);
+        console.log(todos)
+
+    }
+}
+
+
+const botonAgregar = document.querySelector('.nueva-tarea button');
+
+botonAgregar.addEventListener('click', function() {
+
+    agregarTodo();
+    setTimeout(() => {
+        renderizarTodos();
+    }, 4000);
+});
+
+
+renderizarTodos();
+
+
+/* 
+function getPosts(success, error) {
+var req = new XMLHttpRequest();
+    req.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+    req.onload = function() {
+      if (req.status == 200) {
+        success(JSON.parse(req.response));
+      }
+      else {
+        error();
+      }
+    };
+
+    req.send();
+}
+
+getPosts(
+    function(r) {
+        console.log(r)
+    },
+    function(r) {
+        console.log('Ocurrió un error')
+    },
+)
+<li class="tarea">
         <div class="not-done"></div>
         <div class="descripcion">
           <p class="nombre">Mi hermosa tarea</p>
           <p class="timestamp">Creada: 19/04/20</p>
         </div>
       </li> */
-
-
-const tareasPendientes = document.querySelector(".tareas-pendientes");
-
-function renderizarTodos() {
-    // tareasPendientes.innerHTML
-
-    //for (let i = 0; i < listadoTodos.length; i++) {
-    listadoTodos.forEach((todo) => {
-        tareasPendientes.innerHTML += `<li class="tarea">
-        <div class="not-done"></div>
-        <div class="descripcion">
-        <p class="nombre">${todo.description}</p>
-        <p class="timestamp">${todo.createdAt}</p>
-        </div>
-        </li>
-        `;
-    })
-
-    //  console.log("deberiamos renderizar todas las todos del array");
-}
-
-renderizarTodos();
